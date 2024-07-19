e92f0eff6db9ad95ba8f1b9adecb5904dd9244ac
gokul11122 <gokul@wowmedia.cloud>
2024-07-19 09:18:42 +0530

    init

--- app/page.js
+++ app/page.js
@@ -1,95 +1,50 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { setup } from '@loomhq/record-sdk';
+import { isSupported } from '@loomhq/record-sdk/is-supported';
+import { oembed } from '@loomhq/loom-embed';
+import { useEffect, useState } from 'react';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const PUBLIC_APP_ID = 'd17a808b-7bec-41c2-a497-5e08f8c21f33';
+const BUTTON_ID = 'loom-record-sdk-button';
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default function App() {
+    const [videoHTML, setVideoHTML] = useState('');
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    useEffect(() => {
+        async function setupLoom() {
+            const { supported, error } = await isSupported();
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            if (!supported) {
+                console.warn(`Error setting up Loom: ${error}`);
+                return;
+            }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+            const button = document.getElementById(BUTTON_ID);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            if (!button) {
+                return;
+            }
+            const config = { allowedRecordingTypes: 'cam', defaultRecordingType: 'cam' };
+            const { configureButton } = await setup({
+                publicAppId: PUBLIC_APP_ID,
+                config,
+            });
+
+            const sdkButton = configureButton({ element: button });
+
+            sdkButton.on('insert-click', async (video) => {
+                const { html } = await oembed(video.sharedUrl, { width: 400 });
+                setVideoHTML(html);
+            });
+        }
+
+        setupLoom();
+    }, []);
+
+    return (
+        <>
+            <button id={BUTTON_ID}>Record</button>
+            <div dangerouslySetInnerHTML={{ __html: videoHTML }}></div>
+        </>
+    );
 }
